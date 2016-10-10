740dfd7b6e38c41ac67b960f4d8f375d7136f830
Tino Groteloh <tino.groteloh@hoerburger.de>
2016-10-10 13:45:00 +0200

    first try

--- gulpfile.js
+++ gulpfile.js
@@ -2,22 +2,11 @@ var Gulp = require('gulp');
 var Autoprefixer = require('gulp-autoprefixer');
 var BrowserSync = require('browser-sync').create();
 var Sass = require('gulp-sass');
-var Clean = require('gulp-clean');
 
 const config = {
     destFolder: './dist',
     sourceFolder: './src'
 };
-
-Gulp.task('browser-sync', ['styles', 'html'], () =>
-    BrowserSync.init({
-        server: config.destFolder
-    })
-);
-
-Gulp.task('browsersync-reload', () =>
-    BrowserSync.reload()
-);
 
 Gulp.task('styles', () =>
     Gulp.src(config.sourceFolder + '/scss/circle.scss')
@@ -27,19 +16,18 @@ Gulp.task('styles', () =>
         .pipe(BrowserSync.stream({match: '**/*.css'}))
 );
 
-Gulp.task('html', ['clean', 'styles'], () =>
+Gulp.task('html', ['styles'], () =>
     Gulp.src(config.sourceFolder + '/index.html')
         .pipe(Gulp.dest(config.destFolder))
 );
 
-Gulp.task('clean', () =>
-    Gulp.src(config.destFolder, {read: false})
-        .pipe(Clean())
-);
+Gulp.task('default', ['styles', 'html']);
 
-Gulp.task('default', ['clean', 'styles', 'html']);
+Gulp.task('watch', ['styles', 'html'], () => {
+    BrowserSync.init({
+        server: config.destFolder
+    });
 
-Gulp.task('watch', ['clean', 'styles', 'html', 'browser-sync'], () => {
     Gulp.watch(config.sourceFolder + '/scss/**', ['styles']);
-    Gulp.watch(config.sourceFolder + '/**/*.html', ['html', 'browsersync-reload']);
+    // Gulp.watch(config.sourceFolder + '/*.html', ['html', BrowserSync.reload]);
 });
